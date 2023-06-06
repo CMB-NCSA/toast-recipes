b7b8fbd0be71be37e4ffc4b92c5501cd77f78313
Felipe Menanteau <felipe@illinois.edu>
2023-06-06 17:24:23 -0500

    Add TAG to build

--- build_docker.sh
+++ build_docker.sh
@@ -1,10 +1,11 @@
 export CMBS4USER=cmbs4user
 export IMAGE=menanteau/toast_ubuntu
 export TOAST_VERSION=0f8e6b5
-docker build -f docker/Dockerfile -t $IMAGE:$TOAST_VERSION --build-arg CMBS4USER --build-arg TOAST_VERSION --rm=true .
+export TAG=${TOAST_VERSION}_v2
+docker build -f docker/Dockerfile -t $IMAGE:$TAG --build-arg CMBS4USER --build-arg TOAST_VERSION --rm=true .
 
 echo 'Push commands:'
-echo "   docker push $IMAGE:${TOAST_VERSION}"
+echo "   docker push $IMAGE:${TAG}"
 
 echo 'To create singularity image:'
-echo "  ./docker2singularity $IMAGE:${TOAST_VERSION}"
+echo "  ./docker2singularity $IMAGE:${TAG}"
